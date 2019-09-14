e44a0c6e5444fe52b1524d95acae93b5fafb360b
Yuriy Bush <yurachi@gmail.com>
2019-09-14 18:19:25 +0100

    Added unit tests to ensure the robot can be created and that the direction of new robot is undefined

---
+++ Tests/Robot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simulator;
+
+namespace Tests
+{
+    [TestClass]
+    public class Robot
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            var o = new Simulator.Robot();
+            Assert.IsNotNull(o);
+        }
+
+        [TestMethod]
+        public void TestNewRobotHasNoDirection()
+        {
+            var o = new Simulator.Robot();
+            Assert.AreEqual(o.Face, Direction.NONE);
+        }
+    }
+}
