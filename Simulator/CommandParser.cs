bf6c52eb41aef1d799342727c49d7c566d6ce25d
Yuriy Bush <yurachi@gmail.com>
2019-09-14 16:59:23 +0100

    Added PLACE parameters parsing

--- Simulator/CommandParser.cs
+++ Simulator/CommandParser.cs
@@ -12,10 +12,27 @@ namespace Simulator
         public CommandType Parse(string command)
         {
             var splitCommand = command.Split(" ");
-            var result = CommandType.NOP;
-            if (!Enum.TryParse(splitCommand[0], true, out result)) return CommandType.NOP;
-            return result;
+            if (!Enum.TryParse(splitCommand[0], true, out CommandType result))
+                return CommandType.NOP;
+            if (result != CommandType.PLACE)
+                return result;
+            if (splitCommand.Length == 2 && ParsePlaceParameters(splitCommand[1]))
+                return result;
+            return CommandType.NOP;
         }
 
+        private bool ParsePlaceParameters(string parameters)
+        {
+            var splitParameters = parameters.Split(",");
+            if (splitParameters.Length != 3)
+                return false;
+            if (!int.TryParse(splitParameters[0], out int xResult))
+                return false;
+            if (!int.TryParse(splitParameters[1], out int yResult))
+                return false;
+            if (!Enum.TryParse(splitParameters[2], out Direction dResult))
+                return false;
+            return true;
+        }
     }
 }
