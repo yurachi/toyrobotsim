21a85dce7e40055dfc1f94920aab3035971cd964
Yuriy Bush <yurachi@gmail.com>
2019-09-14 22:33:18 +0100

    Add a test to check that Move does not change direction

--- Tests/Executor.cs
+++ Tests/Executor.cs
@@ -17,5 +17,21 @@ namespace Tests
             robot.Received(1).Y = 0;
             robot.Received(1).Face = Direction.NORTH;
         }
+
+        [TestMethod]
+        public void TestExecutorSetsCoordinatesAndNotDirectionOnMove()
+        {
+            var robot = Substitute.For<IRobot>();
+            robot.X.Returns(0);
+            robot.Y.Returns(0);
+            robot.Face.Returns(Direction.NORTH);
+            var o = new Simulator.Executor(robot);
+
+            o.Execute(CommandType.MOVE);
+
+            robot.Received(1).X = 0;
+            robot.Received(1).Y = 1;
+            robot.Received(1).Face = Direction.NORTH;
+        }
     }
 }
