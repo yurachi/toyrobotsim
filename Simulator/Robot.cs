ed44f75063e0818dcfd88f4c29ab4a52f72622e8
Yuriy Bush <yurachi@gmail.com>
2019-09-14 17:04:45 +0100

    Added class to represent the robot

---
+++ Simulator/Robot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Simulator
+{
+    public class Robot
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Direction Face { get; set; }
+        public bool IsPlaced { get; set; }
+    }
+}
