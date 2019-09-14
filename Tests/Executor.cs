4705eaca8f56f1367a5af528400e698d417cf347
Yuriy Bush <yurachi@gmail.com>
2019-09-14 19:27:44 +0100

    added Place test for Executor

---
+++ Tests/Executor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using Simulator;
+
+namespace Tests
+{
+    [TestClass]
+    public class Executor
+    {
+        [TestMethod]
+        public void TestExecutorSetsCoordinatesAndDirectionOnPlace()
+        {
+            var robot = Substitute.For<IRobot>();
+            var o = new Simulator.Executor(robot);
+            o.Execute(CommandType.PLACE,0,0,Direction.NORTH);
+            robot.Received(1).X = 0;
+            robot.Received(1).Y = 0;
+            robot.Received(1).Face = Direction.NORTH;
+        }
+    }
+}
