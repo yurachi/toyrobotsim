12ff91d57970c51099081ff7d531f79ff0b4a1d9
Yuriy Bush <yurachi@gmail.com>
2019-09-14 16:22:47 +0100

    Added tests for incorrect PLACE command

--- Tests/CommandParser.cs
+++ Tests/CommandParser.cs
@@ -36,5 +36,65 @@ namespace Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void TestPlaceCommandWithoutParameters()
+        {
+            var o = new Simulator.CommandParser();
+            var command = "PLACE";
+            var expected = Simulator.CommandType.NOP;
+
+            var actual = o.Parse(command);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestPlaceCommandWithInsufficentParameters()
+        {
+            var o = new Simulator.CommandParser();
+            var command = "PLACE 0,NORTH";
+            var expected = Simulator.CommandType.NOP;
+
+            var actual = o.Parse(command);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestPlaceCommandWithMissingParameters()
+        {
+            var o = new Simulator.CommandParser();
+            var command = "PLACE 0,,NORTH";
+            var expected = Simulator.CommandType.NOP;
+
+            var actual = o.Parse(command);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestPlaceCommandWithWrongOrderParameters()
+        {
+            var o = new Simulator.CommandParser();
+            var command = "PLACE 0,NORTH,0";
+            var expected = Simulator.CommandType.NOP;
+
+            var actual = o.Parse(command);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestPlaceCommandWithInvalidParameters()
+        {
+            var o = new Simulator.CommandParser();
+            var command = "PLACE 0,0,INVALID";
+            var expected = Simulator.CommandType.NOP;
+
+            var actual = o.Parse(command);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
