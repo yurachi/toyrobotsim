2be4d80caa96259b52f52511760faba06a3fd578
Yuriy Bush <yurachi@gmail.com>
2019-09-14 17:26:05 +0100

    Added flow controller

---
+++ Simulator/Dispatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simulator
+{
+    public class Dispatcher
+    {
+        public Robot Unit { get; set; }
+
+        public CommandParser Parser { get; set; }
+
+        public Func<string> ReadLine { get; set; }
+
+        public Dispatcher(Robot r, CommandParser p)
+        {
+            this.Unit = r;
+            this.Parser = p;
+            this.ReadLine = Console.ReadLine;
+        }
+
+        public void MainLoop()
+        {
+            for (var nextCommand = this.ReadLine(); !string.IsNullOrEmpty(nextCommand); nextCommand = this.ReadLine())
+            {
+                Process(nextCommand);
+            }
+        }
+
+        public void Process(string command)
+        {
+
+        }
+
+
+    }
+}
